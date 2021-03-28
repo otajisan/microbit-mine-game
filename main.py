73e06134fcbda70c8d1b6c27ed9947d14d9c9058
Taji <taji.taji.otajisan@gmail.com>
2021-03-28 15:32:26 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.py
@@ -0,0 +1,60 @@
+music.start_melody(music.built_in_melody(Melodies.NYAN))
+
+def on_button_pressed_a():
+    hero.turn(Direction.RIGHT, 90)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    hero.move(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def get_pos():
+    return {
+        "x": hero.get(LedSpriteProperty.X),
+        "y": hero.get(LedSpriteProperty.Y),
+    }
+
+def get_mines():
+    mine_list = [
+        {"x": 2, "y":0},
+        {"x": 3, "y":3},
+    ]
+
+    return mine_list
+
+def is_goal():
+    current = get_pos()
+    if current["x"] == 4 and current["y"] == 4:
+        return True
+    return False
+
+def is_hit_mine():
+    current = get_pos()
+    mines = get_mines()
+    for mine in mines:
+        if current["x"] == mine["x"] and current["y"] == mine["y"]:
+            return True
+    return False
+
+hero: game.LedSprite = None
+current_pos: List[number] = []
+hero = game.create_sprite(0, 0)
+
+def on_forever():
+    if is_hit_mine():
+        basic.show_animation("""
+        . . . # #
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        """)
+        music.start_melody(music.built_in_melody(Melodies.WAWAWAWAA))
+        basic.show_string("GAME OVER...")
+        music.start_melody(music.built_in_melody(Melodies.PUNCHLINE))
+    elif is_goal():
+        basic.show_icon(IconNames.HEART)
+        music.start_melody(music.built_in_melody(Melodies.WEDDING))
+        basic.show_string("GOAL!!")
+
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,71 @@
+music.startMelody(music.builtInMelody(Melodies.Nyan))
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    hero.turn(Direction.Right, 90)
+})
+//     music.start_melody(music.built_in_melody(Melodies.POWER_UP))
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    hero.move(1)
+})
+function get_pos() {
+    return {
+        "x" : hero.get(LedSpriteProperty.X),
+        "y" : hero.get(LedSpriteProperty.Y),
+    }
+}
+
+function get_mines() {
+    let mine_list = [ {
+        "x" : 2,
+        "y" : 0,
+    }
+    , {
+        "x" : 3,
+        "y" : 3,
+    }
+    ]
+    return mine_list
+}
+
+function is_goal(): boolean {
+    let current = get_pos()
+    if (current["x"] == 4 && current["y"] == 4) {
+        return true
+    }
+    
+    return false
+}
+
+function is_hit_mine(): boolean {
+    let current = get_pos()
+    let mines = get_mines()
+    for (let mine of mines) {
+        if (current["x"] == mine["x"] && current["y"] == mine["y"]) {
+            return true
+        }
+        
+    }
+    return false
+}
+
+let hero : game.LedSprite = null
+let current_pos : number[] = []
+hero = game.createSprite(0, 0)
+basic.forever(function on_forever() {
+    if (is_hit_mine()) {
+        basic.showAnimation(`
+        . . . # #
+        . . # . .
+        . # # # .
+        # # # # #
+        . # # # .
+        `)
+        music.startMelody(music.builtInMelody(Melodies.Wawawawaa))
+        basic.showString("GAME OVER...")
+        music.startMelody(music.builtInMelody(Melodies.Punchline))
+    } else if (is_goal()) {
+        basic.showIcon(IconNames.Heart)
+        music.startMelody(music.builtInMelody(Melodies.Wedding))
+        basic.showString("GOAL!!")
+    }
+    
+})
